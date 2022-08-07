c9d076174ae675e7f8fb838bb7e38867a68733ab
maicon_cardoso <maiconscardoso@hotmail.com>
2022-08-07 10:06:12 -0300

    criacao do metodo findById na camada de service e controle

--- src/main/java/academy/cardoso/springboot/controller/AnimeController.java
+++ src/main/java/academy/cardoso/springboot/controller/AnimeController.java
@@ -4,7 +4,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,9 +33,14 @@ public class AnimeController {
     // }
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-        return service.listAll();
+        return new ResponseEntity<>(service.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Integer id) {
+        return new ResponseEntity<Anime>(service.findById(id), HttpStatus.OK);
     }
 
 }
--- src/main/java/academy/cardoso/springboot/service/AnimeService.java
+++ src/main/java/academy/cardoso/springboot/service/AnimeService.java
@@ -2,16 +2,27 @@ package academy.cardoso.springboot.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import academy.cardoso.springboot.domain.Anime;
 
 @Service
 public class AnimeService {
 
+    private List<Anime> anime = List.of(new Anime(1, "Boku no Hero"), new Anime(2, "Tate no Yusha no Nariagari"),
+    new Anime(3, "Dragon Quest: Adventure of Dai"));
+
     public List<Anime> listAll() {
-        return List.of(new Anime(1, "Boku no Hero"), new Anime(2, "Tate no Yusha no Nariagari"),
-        new Anime(3, "Dragon Quest: Adventure of Dai"));
+        return anime;
+    }
+
+    public Anime findById(Integer id) {
+        return anime.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime id not found"));
     }
     
 }

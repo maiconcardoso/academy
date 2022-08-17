b8617dd31d563b0c9ca6e52233fab4bbce6b9362
maicon_cardoso <maiconscardoso@hotmail.com>
2022-08-17 07:28:11 -0300

    Inserção da annotation @Builder na classe AnimePostRequestBody

--- src/main/java/academy/cardoso/springboot/request/AnimePostRequestBody.java
+++ src/main/java/academy/cardoso/springboot/request/AnimePostRequestBody.java
@@ -2,9 +2,11 @@ package academy.cardoso.springboot.request;
 
 import javax.validation.constraints.NotEmpty;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
     
     @NotEmpty(message = "The anime name cannot be empty")

d1e67b1ba5a4943078f878b17817b5d0525dc7b0
maicon_cardoso <maiconscardoso@hotmail.com>
2022-08-23 12:47:49 -0300

    Comentando linhas do código para executar teste de integração

--- src/test/java/academy/cardoso/springboot/SpringbootEssentialsApplicationTests.java
+++ src/test/java/academy/cardoso/springboot/SpringbootEssentialsApplicationTests.java
@@ -3,10 +3,10 @@ package academy.cardoso.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringbootEssentialsApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 

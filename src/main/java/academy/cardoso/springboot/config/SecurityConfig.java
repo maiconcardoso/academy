17ccf08636d538af0c729a1a858d1e5734b0ae47
maicon_cardoso <maiconscardoso@hotmail.com>
2022-08-24 20:56:50 -0300

    Autenticação com usuário no banco de dados

--- src/main/java/academy/cardoso/springboot/config/SecurityConfig.java
+++ src/main/java/academy/cardoso/springboot/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package academy.cardoso.springboot.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,17 +13,23 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import academy.cardoso.springboot.service.AcademyUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Log4j2
+@RequiredArgsConstructor
 public class SecurityConfig{
+
+    private final AcademyUserDetailsService academyUserDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -43,23 +51,33 @@ public class SecurityConfig{
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration
             ) throws Exception {
-        log.info("Password Encoder {} ", passwordEncoder().encode("test"));
+        log.info("Password Encoder {} ", passwordEncoder().encode("academy"));
         return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService() throws Exception {
+        
         UserDetails user = User.builder()
-            .username("maicon")
+            .username("maicon2")
             .password(passwordEncoder().encode("test"))
             .roles("USER")
             .build();
         UserDetails admin = User.builder()
-            .username("admin")
+            .username("admin2")
             .password(passwordEncoder().encode("test"))
             .roles("USER", "ADMIN")
             .build();
+
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(academyUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
 }
